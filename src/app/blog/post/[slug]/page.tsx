62db027cbb3188409b54ec85546c39083cf96d51
Edward Opoku-Agyemang <oaedward83@gmail.com>
2023-06-16 13:51:42 +0000

    save

--- src/app/blog/post/[slug]/page.tsx
+++ src/app/blog/post/[slug]/page.tsx
@@ -43,9 +43,9 @@ export default async function Post({ params: { slug } }: Props) {
       id="blog"
       className="w-full xl:pt-20 p-4 flex items-center border-b-[1px] border-b-gray-300 dark:border-b-gray-500"
     >
-      <div class="container px-6 py-10 mx-auto">
-        <div class="lg:flex lg:-mx-6">
-          <div class="lg:w-3/4 lg:px-6">
+      <div className="container px-6 py-10 mx-auto">
+        <div className="lg:flex lg:-mx-6">
+          <div className="lg:w-3/4 lg:px-6">
             <div className="w-full h-80 xl:h-[28rem] relative">
               <Image
                 className="object-cover object-center rounded-lg"
@@ -56,11 +56,11 @@ export default async function Post({ params: { slug } }: Props) {
             </div>
 
             <div>
-              <h1 class="max-w-lg mt-4 text-2xl font-semibold leading-tight text-gray-800 dark:text-white">
+              <h1 className="max-w-lg mt-4 text-2xl font-semibold leading-tight text-gray-800 dark:text-white">
                 {post.title}
               </h1>
 
-              <div class="flex items-center mt-6">
+              <div className="flex items-center mt-6">
                 <Image
                   className="rounded-full"
                   src={urlForImage(post.author.image).url()}
@@ -69,11 +69,11 @@ export default async function Post({ params: { slug } }: Props) {
                   height={20}
                 />
 
-                <div class="mx-4">
-                  <h1 class="text-sm text-gray-700 dark:text-gray-200">
+                <div className="mx-4">
+                  <h1 className="text-sm text-gray-700 dark:text-gray-200">
                     {post.author.name}
                   </h1>
-                  <p class="text-sm text-gray-500 dark:text-gray-400">
+                  <p className="text-sm text-gray-500 dark:text-gray-400">
                     {new Date(post._createdAt).toLocaleDateString("en-UK", {
                       day: "numeric",
                       month: "long",
